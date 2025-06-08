db611449194fdc7c59e0e510f39b785f2ff7252f
Atul Kamble <atul_kamble@hotmail.com>
2025-06-08 09:24:40 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,16 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
   tags = {
     Name = "${var.cluster_name}-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.cluster_name}-igw"
   }
 }
 
@@ -16,4 +25,24 @@ resource "aws_subnet" "public" {
   }
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.cluster_name}-public-rt"
+  }
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
 data "aws_availability_zones" "available" {}
+
